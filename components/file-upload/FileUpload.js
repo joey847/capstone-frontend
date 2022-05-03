407b05749883e6011c90f59fe05bfdc1c7aac17b
joey847 <barlowjoseph847@outlook.com>
2022-05-03 00:18:54 -0400

    File name is shown after selecting

--- components/file-upload/FileUpload.js
+++ components/file-upload/FileUpload.js
@@ -16,7 +16,9 @@ class FileUpload extends React.Component {
     fileHandler = async (event) => {
         this.setState({
             selectedFile: event.target.files[0]
-        })
+        });
+
+        document.getElementById("file-upload-text").innerHTML = `Upload <span class="italic underline truncate">${event.target.files[0].name}</span>`;
     }
 
     fileName = "";
@@ -52,7 +54,7 @@ class FileUpload extends React.Component {
 
         // The file that we want to upload
         const formData = new FormData();
-
+        
         if (this.state.selectedFile === null) {
             toast.error('File is required', { id: 'no-file-toast' },);
             return;
@@ -90,20 +92,21 @@ class FileUpload extends React.Component {
             {/* I added some responsive elements to the form to improve breakpoints */}
             <div className='xs:w-full sm:w-full mt-14 p-8 border border-black bg-red-50 border-2 border-dashed lg:1/4 md:w-2/5 flex justify-center item-center'>
                 <div className='flex flex-col'>
-            <DateValue/>
                     
-                    <label className="border border-black hover:bg-gray-50 hover:border-gray-300">
-                        <div className="flex flex-col items-center justify-center pt-7">
+                    <DateValue/>
+                    
+                    <label className="max-w-sm border border-black hover:bg-gray-50 hover:border-gray-300">
+                        <div className="max-w-sm flex flex-col items-center justify-center pt-7">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-gray-400 group-hover:text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                             </svg>
-                            <p className="pt-1 text-black text-lg text-center group-hover:text-gray-600">Select Files to Upload</p>
+                            <p id="file-upload-text" className="max-w-xs truncate pt-1 text-black text-lg text-center group-hover:text-gray-600">Select Files to Upload</p>
                         </div>
                         
                         <input type="file" onChange={this.fileHandler} className="opacity-0" />
                     </label>
                     
-                    <button onClick={this.fileUploadHandler} className='bg-white border border-black text-black font-semibold py-2 px-4 mt-8 inline-flex justify-center items-center drop-shadow hover:drop-shadow-none hover:bg-gray-50 hover:border-gray-300'>
+                    <button onClick={this.fileUploadHandler} className='max-w-sm bg-white border border-black text-black font-semibold py-2 px-4 mt-8 inline-flex justify-center items-center drop-shadow hover:drop-shadow-none hover:bg-gray-50 hover:border-gray-300'>
                         <span>Upload</span>
                     </button>
                 </div>
